285e7e0ca90acf4e999aac3f4a3837098c995d75
1n2a3o4y5a <user.id.125125@gmail.com>
2020-08-25 15:16:48 +0900

    favorite

--- my_app/lib/main.dart
+++ my_app/lib/main.dart
@@ -6,6 +6,7 @@ void main() => runApp(MyApp());
 class RandomWordsState extends State<RandomWords> {
   @override
   final _suggestions = <WordPair>[];
+  final _saved = Set<WordPair>();
   final _biggerFont = const TextStyle(fontSize: 18.0);
   Widget build(BuildContext context) {
     // final wordPair = WordPair.random();
@@ -31,11 +32,25 @@ class RandomWordsState extends State<RandomWords> {
         });
   }
   Widget _buildRow(WordPair pair) {
+    final bool alreadySaved = _saved.contains(pair);
     return ListTile(
       title: Text(
         pair.asPascalCase,
         style: _biggerFont,
       ),
+      trailing: Icon(   // NEW from here... 
+        alreadySaved ? Icons.favorite : Icons.favorite_border,
+        color: alreadySaved ? Colors.red : null,
+      ),
+      onTap: () {      // NEW lines from here...
+        setState(() {
+          if (alreadySaved) {
+            _saved.remove(pair);
+          } else { 
+            _saved.add(pair); 
+          } 
+        });
+      }, 
     );
   }
 }
