951411f5802494c2ff0446aae7cee699c8d46286
1n2a3o4y5a <user.id.125125@gmail.com>
2020-08-26 17:55:55 +0900

    form

--- my_app2/lib/main.dart
+++ my_app2/lib/main.dart
@@ -5,6 +5,11 @@ void main() {
   runApp(MyApp());
 }
 
+final myFocusNode = FocusNode();
+String name;
+final myController = TextEditingController();
+
+
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
@@ -43,7 +48,32 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text('最初の画面'),
       ),
       body: Container(
-
+        width: double.infinity,
+        child: Column(
+          children: [
+            TextField(
+              autofocus: true,
+              decoration: InputDecoration(
+                hintText:'田中太郎'
+              ),
+              onChanged: (text) {
+                name = text;
+                print("First text field: $text");
+              },
+            ),
+            TextField(
+              controller: myController,
+              decoration: InputDecoration(
+                  hintText:'趣味',
+              ),
+            ),
+            RaisedButton(
+              child: Text('新規登録'),
+              onPressed: () {
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
